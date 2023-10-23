277e6c2f02ea1a5480ec7d17f1782b07f8d78db1
Radoncode <relbouni69@gmail.com>
2023-10-23 21:44:47 +0100

    adding heroicons

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,10 +1,47 @@
-
+import { Square3Stack3DIcon, CurrencyPoundIcon, MinusIcon, ScaleIcon  } from '@heroicons/react/24/solid'
+import { Fragment } from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1>here home</h1>
-        </div>
+        <Fragment>
+            <div className="grid grid-cols-4 gap-4 my-8">
+                <div className="rounded-lg flex flex-col items-center justify-center shadow hover:shadow-2xl shadow-lg">
+                    <Square3Stack3DIcon className="h-16 w-16 text-blue-500" /> 
+                    <p>Area</p>
+                </div>
+                <div className="rounded-lg flex flex-col items-center justify-center shadow hover:shadow-2xl shadow-lg">
+                    <CurrencyPoundIcon className="h-16 w-16 text-blue-500" />
+                    <p>Currency</p>
+                </div>
+                <div className="rounded-lg flex flex-col items-center justify-center shadow hover:shadow-2xl shadow-lg">
+                    <MinusIcon className='h-16 w-16 text-blue-500' />
+                    <p>Length</p>
+                </div>
+                <div className="rounded-lg flex flex-col items-center justify-center shadow hover:shadow-2xl shadow-lg">
+                    <ScaleIcon className='h-16 w-16 text-blue-500' />
+                    <p>Weight</p>
+                </div>
+            </div>
+            <div className="grid grid-cols-4 gap-4">
+                <div className="rounded-lg flex flex-col items-center justify-center shadow hover:shadow-2xl shadow-lg">
+                    <Square3Stack3DIcon className="h-16 w-16 text-blue-500" /> 
+                    <p>Area</p>
+                </div>
+                <div className="rounded-lg flex flex-col items-center justify-center shadow hover:shadow-2xl shadow-lg">
+                    <CurrencyPoundIcon className="h-16 w-16 text-blue-500" />
+                    <p>Currency</p>
+                </div>
+                <div className="rounded-lg flex flex-col items-center justify-center shadow hover:shadow-2xl shadow-lg">
+                    <MinusIcon className='h-16 w-16 text-blue-500' />
+                    <p>Length</p>
+                </div>
+                <div className="rounded-lg flex flex-col items-center justify-center shadow hover:shadow-2xl shadow-lg">
+                    <ScaleIcon className='h-16 w-16 text-blue-500' />
+                    <p>Weight</p>
+                </div>
+            </div>     
+        </Fragment>
+        
     )
 }
 
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,43 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import logo from '../../converter.svg';
 
 
 const Navigation = () => {
     return (
-        <div className="container mx-auto">
-            <div>
-                HERE THE NAVIGATION
+        <div className="container mx-auto px-4">
+            <div className="flex justify-between items-center">
+                <Link to="/" className="block p-6">
+                    <img src={logo}  alt="logo converter" />
+                </Link>
+                <ul  className="flex space-x-10">
+                    <li>
+                        <Link to="/currency" className="block border-solid border-1 p-6">
+                            Currency
+                        </Link>
+                    </li>
+                    <li >
+                        <Link to="/length" className="block border-solid border-1 p-6">
+                            Length
+                        </Link>
+                    </li>
+                    <li >
+                        <Link to="/weight" className="block border-solid border-1 p-6">
+                            Weight
+                        </Link>    
+                    </li>
+                    <li >
+                        <Link to="/volume" className="block border-solid border-1 p-6">
+                            Volume
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/area" className="block border-solid border-1 p-6">
+                            Area
+                        </Link>
+                    </li>
+                </ul>                          
             </div>
-            <div>
+            <div className="px-3">
                 <Outlet />
             </div>
         </div>
